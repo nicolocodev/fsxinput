bc87e646218e3034c66353b6d9d1907c7f380267
nicolocodev <nicoherreraf@hotmail.com>
2014-11-17 17:00:30 -0500

    added Guide button support and turn off controller

--- src/Demo/Program.fs
+++ src/Demo/Program.fs
@@ -3,14 +3,38 @@ open System
 
 [<EntryPoint>]
 let main argv =
+
     let controller = Xinput.Controllers.Controller(0)
     let isConnected = controller.IsConnected
-    if isConnected then printfn "%s" "Connected control"
-    else printf "%s" "Disconnected control"
-    controller.OnButtonPress.Add(fun args -> printfn "%A" args.ControllerButton)
-    //You can use observable to query the events stream
-    //let observable = controller.OnButtonPress
-    //observable |> Observable.filter(fun args -> args.ControllerButton = ControllerButtons.A) |> Observable.subscribe(fun _ -> controller.Vibrate 30000us 3000us) |> ignore
-    controller.Start    
+
+    let startUsingController() =
+
+        printfn "%s" "Connected control"
+
+        controller.OnDisconnect.Add(fun _ -> 
+            Console.WriteLine("Disconnected control - Event")
+            Console.ReadLine() |> ignore)
+
+        //You can use observable to query the events stream
+        let observable = controller.OnButtonPress    
+
+        let buttonPress = 
+            observable 
+            |> Observable.subscribe(fun b -> printfn "%A" b.ControllerButton)                         
+        
+        //Press Guide button to turn off
+        let guideButton = 
+            observable 
+            |> Observable.filter(fun args -> args.ControllerButton = ControllerButtons.Guide) 
+            |> Observable.subscribe(fun _ -> controller.Turnoff 0)             
+
+        controller.Start 100.0
+
+    if isConnected then 
+        startUsingController()
+    else 
+        printf "%s" "Disconnected control"
+        Console.ReadLine() |> ignore
+    
     Console.ReadLine() |> ignore
     0 // return an integer exit code
--- src/FsXinput/FsXinput.fs
+++ src/FsXinput/FsXinput.fs
@@ -19,6 +19,7 @@ type ControllerButtons =
     | B = 0x2000
     | X = 0x4000
     | Y = 0x8000
+    | Guide = 0x0400
     | None = 0
 
 type Vibatrion = 
@@ -45,52 +46,66 @@ type State =
     end
 
 [<SuppressUnmanagedCodeSecurity>]
-[<DllImport("xinput1_4.dll", EntryPoint="XInputGetState", CharSet=CharSet.Auto, CallingConvention=CallingConvention.Winapi)>]
+[<DllImport("xinput1_4.dll", EntryPoint="#100", CharSet=CharSet.Auto, CallingConvention=CallingConvention.Winapi)>]
 extern int internal GetState(int dwUserIndex, State& pState)
 
 [<SuppressUnmanagedCodeSecurity>]
 [<DllImport("xinput1_4.dll", EntryPoint="XInputSetState", CharSet=CharSet.Auto, CallingConvention=CallingConvention.Winapi)>]
 extern int internal SetState(int dwUserIndex, Vibatrion& pVibration)
 
+[<SuppressUnmanagedCodeSecurity>]
+[<DllImport("xinput1_4.dll", EntryPoint="#103", CharSet=CharSet.Auto, CallingConvention=CallingConvention.Winapi)>]
+extern int internal TurnOff(int dwUserIndex)
+
+
 type ButtonEventArgs(controllerButton) =
     let button = controllerButton
     with member this.ControllerButton = button        
 
+type DisconnectEventArgs(playerIndex) =    
+    let index = playerIndex
+    with member this.ControllerButton = index
+
 type Controller(playerIndex:int) =     
     let _playerIndex = playerIndex    
     let mutable state = new State()
-    let mutable vibration = new Vibatrion()
-    let mutable pollInterval = 100
+    let mutable vibration = new Vibatrion()    
 
     let onButtonPress = new Event<ButtonEventArgs>()
-    let trigger button = onButtonPress.Trigger (new ButtonEventArgs(button))    
-    
-    member this.PollInterval 
-        with get() = pollInterval
-        and set(value) = pollInterval <- value
+    let buttonPressTrigger button = onButtonPress.Trigger (new ButtonEventArgs(button))    
 
-    member this.Start = 
-        let poll =
-            let timer = new System.Timers.Timer(float pollInterval)
+    let onDisconnect = new Event<DisconnectEventArgs>()
+    let disconnectTrigger controller = onDisconnect.Trigger(new DisconnectEventArgs(controller))
+
+    member this.Start (pollInterval:float)= 
+        let timer = new System.Timers.Timer(pollInterval)
+        let poll =            
             timer.AutoReset <- true        
             let observable = timer.Elapsed  
             let task = async {timer.Start()}
             (task,observable)        
-        let tmr, stream = poll
+        let task, stream = poll
         stream |> Observable.subscribe (fun _ -> 
-                    this.PollState |> ignore
+                    if not <| this.PollState then                         
+                        timer.Stop() 
+                        disconnectTrigger _playerIndex
                     match state.GamePad.Buttons with
                     | ControllerButtons.None -> ()                
-                    | _ -> trigger state.GamePad.Buttons) 
+                    | _ -> buttonPressTrigger state.GamePad.Buttons) 
                 |> ignore 
-        Async.RunSynchronously tmr
+        Async.RunSynchronously task
 
+    member this.Turnoff playerIndex =
+        TurnOff(playerIndex) |> ignore
+        ()
 
     [<CLIEvent>]
     member this.OnButtonPress = onButtonPress.Publish      
 
-    member this.PollState =
-        GetState(_playerIndex, &state) = 0
+    [<CLIEvent>]
+    member this.OnDisconnect = onDisconnect.Publish      
+
+    member this.PollState = GetState(_playerIndex, &state) = 0
     
     member this.IsConnected = this.PollState
     
